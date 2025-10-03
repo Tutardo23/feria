97434659cb2feb0a1ab16c669f4d0328176995a3
Tutardo23 <matiastorres2783@gmail.com>
2025-10-03 10:36:44 -0300

    p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Loader from '../components/Loader'
 export default function Home() {
   return (
     <>
-     
+     <Loader />
       <Hero />
       <Eventos />
       <Cursos />  {/* 👈 Aquí se agrega */}
